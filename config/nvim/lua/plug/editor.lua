35f55aeebe871fe3bc1bca6ffa05481597b691ee
Ru-kko <verase2003.1@gmail.com>
2023-05-30 11:00:48 -0500

    feat(nvim): stylng tabs

--- config/nvim/lua/plug/editor.lua
+++ config/nvim/lua/plug/editor.lua
@@ -1,62 +1,95 @@
 return {
-	-- Error highlighting
-	{
+  -- Tabs
+  {
+    "akinsho/bufferline.nvim",
+    after = "catppuccin",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        diagnostics = "nvim_lsp",
+        separator_style = "slant",
+        diagnostics_indicator = function(_, _, diag)
+          local ret = (diag.error and " " .. diag.error .. " " or "")
+              .. (diag.warning and " " .. diag.warning or "")
+          return vim.trim(ret)
+        end,
+      },
+    },
+    keys = {
+      {
+        "<leader><tab>",
+        function()
+          require("bufferline").cycle(1)
+        end,
+        mode = { "n", "i", "v" },
+      },
+      {
+        "<S-tab>",
+        function()
+          require("bufferline").cycle(-1)
+        end,
+        mode = { "n", "i", "v" },
+      },
+    },
+    config = function(_, opts)
+      opts.highlights = require("catppuccin.groups.integrations.bufferline").get({
+        styles = { "italic", "bold" },
+      })
+      require("bufferline").setup(opts)
+    end,
+    dependencies = "nvim-tree/nvim-web-devicons",
+  },
+  -- Error highlighting
+  {
     "folke/trouble.nvim",
     cmd = "Trouble",
-    config = function ()
-    	require('trouble').setup({
-				signs = {
-        	error = "",
-        	warning = "",
-        	hint = "",
-        	information = "",
-        	other = "﫠"
-    		},
-			})
+    config = function()
+      require("trouble").setup({
+        signs = {
+          error = " ",
+          warning = " ",
+          hint = " ",
+          information = " ",
+          other = "﫠",
+        },
+      })
     end,
     dependencies = "nvim-tree/nvim-web-devicons",
     keys = {
       { "<leader>q", "<cmd>Trouble<CR>" },
-  	},
-	},
-	-- Commeter
-	{
-    "danymat/neogen",
-		cmd = 'Neogen',
-    dependencies = "nvim-treesitter/nvim-treesitter",
+    },
+  },
+  -- Code collapse
+  {
+    "stevearc/aerial.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    cmd = {
+      "AerialNavToggle",
+    },
+    keys = {
+      { "<leader>zz", "<cmd>AerialToggle<CR>", mode = "n" },
+    },
+    opts = {
+      layout = {
+        placement = "edge",
+        default_direction = "float",
+      },
+      keymaps = {
+        ["<"] = "actions.tree_decrease_fold_level",
+        [">"] = "actions.tree_increase_fold_level",
+      },
+      on_attach = function(bufnr)
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+      end,
+    },
     config = true,
-	},
-	-- Code collapse
-	{
-  	'stevearc/aerial.nvim',
-  	dependencies = {
-    	"nvim-treesitter/nvim-treesitter",
-			"nvim-tree/nvim-web-devicons"
-  	},
-		cmd = {
-			'AerialNavToggle'
-		},
-		keys = {
-			{'<leader>zz', '<cmd>AerialNavToggle<CR>', mode = "n" },
-		},
-		opts = {
-			layout = {
-				placement = "edge",
-				default_direction = "float"
-			},
-			keymaps = {
-      	["<"] = "actions.tree_decrease_fold_level",
-      	[">"] = "actions.tree_increase_fold_level",
-    	},
-  		on_attach = function(bufnr)
-    		vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', {buffer = bufnr})
-    		vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', {buffer = bufnr})
-  		end
-		},
-		config = true,
-	},
-	-- TreeSitter
-	{
+  },
+  -- TreeSitter
+  {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
@@ -87,6 +120,6 @@ return {
     end,
     config = function()
       require("plug.treesitter")
-    end
+    end,
   },
 }
