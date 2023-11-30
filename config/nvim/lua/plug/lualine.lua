ad07b5d9d1446eaf1637591323b304ed76a18d65
Ru-kko <verase2003.1@gmail.com>
2023-11-30 00:46:49 -0500

    chore: migrate deprecate code

--- config/nvim/lua/plug/lualine.lua
+++ config/nvim/lua/plug/lualine.lua
@@ -1,47 +1,60 @@
-require('lualine').setup {
+require("lualine").setup({
   options = {
     icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    theme = "auto",
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
     disabled_filetypes = {
       statusline = {},
       winbar = {},
     },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+    lualine_a = {
+      { "mode", separator = { right = "", left = " " } },
+    },
+    lualine_b = { "branch", "diff", "diagnostics" },
+    lualine_c = { "filename" },
     lualine_x = {
-			{
-				require("noice").api.statusline.mode.get,
-				cond = require("noice").api.statusline.mode.has,
-				color = "Float"
-			}, 
-			"encoding", "fileformat", "filetype"
-		},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+      "encoding",
+      "fileformat",
+      "filetype",
+    },
+    lualine_y = { "progress" },
+    lualine_z = {
+      { "location", separator = { right = " ", left = "" } },
+    },
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = { "filename" },
+    lualine_x = { "location" },
     lualine_y = {},
-    lualine_z = {}
+    lualine_z = {},
   },
+  -- sections = {
+  --   lualine_a = {
+  --     { "mode", separator = { right = "", left = " " } },
+  --   },
+  --   lualine_b = { "branch", "diff", "diagnostics" },
+  --   lualine_c = { "filename" },
+  --   lualine_x = { "encoding", "fileformat" },
+  --   lualine_y = { "filetype", "progress" },
+  --   lualine_z = {
+  --     { "location", separator = { right = " ", left = "" } },
+  --   },
+  --   inactive_sections = {
+  --     lualine_a = { "filename" },
+  --     lualine_b = {},
+  --     lualine_c = {},
+  --     lualine_x = {},
+  --     lualine_y = {},
+  --     lualine_z = { "location" },
+  --   },
+  -- },
   tabline = {},
   winbar = {},
   inactive_winbar = {},
-  extensions = {}
-}
+  extensions = {},
+})
