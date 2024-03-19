0882b81311d7686c5473d7f868896f687bfebc9d
Ru-kko <verase2003.1@gmail.com>
2024-03-19 18:37:36 -0500

    fix: bugged incons

--- config/nvim/lua/plug/alpha.lua
+++ config/nvim/lua/plug/alpha.lua
@@ -26,9 +26,9 @@ return {
 			dashboard.button("e", "  New file", ":enew<CR>"),
 			dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
 			dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-			dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+			dashboard.button("t", "󰊄  Find text", ":Telescope live_grep <CR>"),
 			dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
-			dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+			dashboard.button("q", "󰅚  Quit Neovim", ":qa<CR>"),
 		}
 
 		local function footer()
