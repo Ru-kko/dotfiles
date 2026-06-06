94d802bc9cfaa56498049dcf29340ac6a9e6b01e
Ru-kko <verase2003.1@gmail.com>
2026-06-05 23:08:34 -0500

    fix(nix): add boot loader configuration

--- host/system.nix
+++ host/system.nix
@@ -7,7 +7,14 @@
     networkmanager.enable = true;
   };
 
-  time.timeZone = "UTC";
+  time.timeZone = "America/Bogota";
   i18n.defaultLocale = "en_US.UTF-8";
   console.keyMap = "la-latin1";
+
+  # Hardware configuration
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+    useOSProber = true;
+  };
 }
